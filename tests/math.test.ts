816e89684a10cdd0b9005972193c7bbb21a3751d
Victor Ng <victor@crankycoder.com>
2024-05-27 01:35:28 -0400

    Stuffed some extra statements into test to exercise debugging

--- tests/math.test.ts
+++ tests/math.test.ts
@@ -2,6 +2,8 @@ import { add } from "../src/math";
 
 describe("Math functions", () => {
   it("should add two numbers correctly", () => {
+    let x = 5;
+    console.log(x);
     expect(add(1, 2)).toEqual(3);
   });
 });
